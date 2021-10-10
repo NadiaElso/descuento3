2ec937551c8119c52ac32cd7176281c445d90735
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-10 19:24:42 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let precioproducto: number = Document.getElementById(precioproducto);
+let cantidad: number = Document.getElementById(cantidad);
+
+let precioproducto: number = number(precioproducto.value);
